b005cc18b72dca83d0e25ba877b2013c97805990
Hossein Ghayebi <nhosseinghayebi@gmail.com>
2024-09-09 07:46:50 +0330

    feat:Add github authentication

--- auth.ts
+++ auth.ts
@@ -1,9 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import NextAuth, { DefaultSession } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
 import { LoginSchema } from "./schemas";
 import { getUserByEmail } from "./data/user";
 import { User } from "@prisma/client";
 import bcrypt from "bcryptjs";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import db from "./db";
 
 export type ExtendedUser = Omit<User, "password"> & DefaultSession["user"];
 
@@ -35,6 +40,14 @@ export const {
     },
   },
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
@@ -68,4 +81,7 @@ export const {
       },
     }),
   ],
+
+  session: { strategy: "jwt" },
+  adapter: PrismaAdapter(db),
 });
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,15 +1,31 @@
+"use client";
+
 import { Button } from "@nextui-org/react";
+import { signIn } from "next-auth/react";
 import React from "react";
 import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 
 export default function Social() {
+  const onClick = async (provider: "google" | "github") => {
+    await signIn(provider);
+  };
   return (
     <div className="flex w-full items-center gap-x-2">
-      <Button fullWidth size="lg" variant="bordered">
+      <Button
+        fullWidth
+        size="lg"
+        variant="bordered"
+        onClick={() => onClick("google")}
+      >
         <FcGoogle className="h-5 w-5" />
       </Button>
-      <Button fullWidth size="lg" variant="bordered">
+      <Button
+        fullWidth
+        size="lg"
+        variant="bordered"
+        onClick={() => onClick("github")}
+      >
         <FaGithub className="h-5 w-5" />
       </Button>
     </div>
